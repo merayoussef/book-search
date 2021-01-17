c233f66f9aecba110d469370ada2e3d0bc4b7be9
Mera Youssef <merayoussef91@hotmail.com>
2021-01-17 15:49:22 -0600

    added typeDefs

--- server/server.js
+++ server/server.js
@@ -2,20 +2,31 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+const {} = require('./schemas')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+    // add schemas
+    context: authMiddleware
+});
 
-app.use(express.urlencoded({ extended: true }));
+//integrate our Apollo server with Express application
+server.applyMiddleware({ app })
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
